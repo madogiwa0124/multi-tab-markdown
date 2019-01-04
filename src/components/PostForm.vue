71df593b93c5c544d7071ca7badf64f52e1428cf
Madogiwa <17684091+Madogiwa0124@users.noreply.github.com>
2019-01-04 15:51:44 +0900

    投稿フォームのscssをリファクタリング

--- src/components/PostForm.vue
+++ src/components/PostForm.vue
@@ -1,6 +1,6 @@
 <template>
   <form id="post-form" @submit="handleOnSubmit">
-    <input type="text" id="post-title"  v-model="title" placeholder="Title">
+    <input type="text" id="title" v-model="title" placeholder="Title">
     <div id="editor">
       <textarea v-model="markdownText">input</textarea>
       <div class="save-button">
@@ -65,51 +65,53 @@
     text-align: right;
   }
 
-  #post-title {
-    border: none;
-    padding:10px;
-    font-size:20px;
-    margin:0 0 10px;
-    width: 100%;
-    outline: none;
-  }
-
   #post-form {
     margin: 0;
     height: 90%;
     width: 100%;
     font-family: 'Helvetica Neue', Arial, sans-serif;
     color: #333;
-  }
 
-  #post-form textarea {
-    width: 100%;
-    height: 95%;
-    border: none;
-    resize: none;
-    outline: none;
-    background-color: #f6f6f6;
-    font-size: 14px;
-    font-family: 'Monaco', courier, monospace;
-    padding: 10px;
-    display: block;
-  }
+    #title {
+      border: none;
+      padding:10px;
+      font-size:20px;
+      margin:0 0 10px;
+      width: 100%;
+      outline: none;
+    }
 
-  #post-form #preview {
-    padding: 20px;
-    height: 95%;
-    width: 50%;
-    padding: 20px;
-    display: inline-block;
-    vertical-align: top;
-    box-sizing: border-box;
-  }
+    textarea {
+      width: 100%;
+      height: 95%;
+      border: none;
+      resize: none;
+      outline: none;
+      background-color: #f6f6f6;
+      font-size: 14px;
+      font-family: 'Monaco', courier, monospace;
+      padding: 10px;
+      display: block;
+    }
 
-  #post-form #editor {
-    padding: 10px;
-    width: 50%;
-    display: inline-block;
-    height: 95%;
-    border-right: 1px solid #ccc;
+    #preview {
+      padding: 20px;
+      max-height: 95%;
+      height: 95%;
+      overflow: scroll;
+      width: 50%;
+      padding: 20px;
+      display: inline-block;
+      vertical-align: top;
+      box-sizing: border-box;
+    }
+
+    #editor {
+      padding: 10px;
+      width: 50%;
+      display: inline-block;
+      height: 95%;
+      border-right: 1px solid #ccc;
+    }
   }
 </style>
